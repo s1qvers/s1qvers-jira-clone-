5c2997308630f90c19fa6fce895fe9ed8004fe89
s1qvers <s1qvers@gmail.com>
2024-12-06 15:11:50 +0600

    Ваше сообщение коммита

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const SignInPage = () => {
+    return <SignInCard />
+};
+
+export default SignInPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Input />
+      <Button size="xs">
+        Primary123
+      </Button>
+      <Button variant="senondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>        
+      <Button variant="teritary">
+        Teritary
+      </Button>
+    </div>
+  )
+}
