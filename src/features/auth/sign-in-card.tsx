74fda6ba01f918fd6ced0ab148e43b509a94b08d
s1qvers <s1qvers@gmail.com>
2024-12-08 00:57:41 +0600

    s1qvers

--- src/features/auth/sign-in-card.tsx
+++ src/features/auth/sign-in-card.tsx
@@ -4,15 +4,19 @@ CardContent,
 CardHeader,   
 CardTitle
 }from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 export const SignInCard = () => {
     return (
         <Card className="w-full h-full md:w-[487] border-none shadow-none">
-            <CardHeader>
-                <CardTitle>
+            <CardHeader className="flex items-center justify-center text-center p-7">
+                <CardTitle className="text-2xl">
                     Welcome back!
                 </CardTitle>
             </CardHeader>
+            <div className="px-7 mb-2">
+                <Separator />
+            </div>
         </Card>
     );
 };
