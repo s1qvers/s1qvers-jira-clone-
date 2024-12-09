107412b1fe485f1ceb6716a1b06244bc5a523be9
s1qvers <s1qvers@gmail.com>
2024-12-10 00:11:28 +0600

    s1qvers

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,26 +1,38 @@
+"use client";
+
+import Image from "next/image"; 
+import Link from "next/link"; 
+import { usePathname } from "next/navigation";
+
 import { Button } from "@/components/ui/button"; 
-import Image from "next/image";
 
 interface AuthLayoutProps {
     children: React.ReactNode;
 };
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+    const pathname = usePathname();
+    const isSignIn = pathname === "/sign-in";
+
     return (
         <main className="bg-neutral-100 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
                 <nav className="flex justify-between items-center">
                     <Image src="/logo.svg" alt="logo" width={152} height={56} />
-                    <Button variant="secondary">
-                        Sign Up
-                    </Button>
-                    </nav>
-                    <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
-                        {children}
-                    </div>
+                    <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                        <Button variant="secondary">
+                            {isSignIn ? "Sign Up" : "Login"}
+                        </Button>
+                    </Link>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                    {children}
                 </div>
-            </main>    
+            </div>
+        </main>    
     );
 }
 
 export default AuthLayout;
+
+
--- src/features/auth/sign-in-card.tsx
+++ src/features/auth/sign-in-card.tsx
@@ -1,5 +1,8 @@
+import { z } from "zod";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
@@ -8,74 +11,113 @@ import {
     CardContent,
     CardHeader,   
     CardTitle
-}from "@/components/ui/card";
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1, "Required"),
+});
 
 export const SignInCard = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log({ values });
+    };
+
     return (
-        <Card className="w-full h-full md:w-[487] border-none shadow-none">
+        <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
                     Welcome back!
                 </CardTitle>
             </CardHeader>
             <div className="px-7">
-                <DottedSeparator  />
+                <DottedSeparator />
             </div>
-        <CardContent className="p-7">
-            <form className="space-y-4">
-                <Input 
-                    required
-                    type="email"
-                    value={""}
-                    onChange={() => {}}
-                    placeholder="Enter email address"
-                    disabled= {false}
-                />
-                <Input 
-                    required
-                    type="password"
-                    value={""}
-                    onChange={() => {}}
-                    placeholder="Enter password "
-                    disabled= {false}
-                    min={8}
-                    max={256}
-                />
-                <Button disabled={false} size="lg" className="w-full">
-                    Login
+            <CardContent className="p-7">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField
+                            name="email"
+                            control={form.control} 
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input   
+                                            {...field}
+                                            type="email"
+                                            placeholder="Enter email address"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control} 
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type="password"
+                                            placeholder="Enter password"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button disabled={false} size="lg" className="w-full">
+                            Login
+                        </Button>
+                    </form>
+                </Form>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7 flex-col gap-y-4">
+                <Button
+                    disabled={false}
+                    variant="secondary"
+                    size="lg"
+                    className="w-full"
+                >
+                    <FcGoogle className="mr-2 size-5" />
+                    Login with Google
                 </Button>
-            </form>   
-        </CardContent>
-        <div className="px-7">
-            <DottedSeparator />
-        </div>
-        <CardContent className="p-7 flex-col gap-y-4">
-            <Button
-                disabled={false}
-                variant="secondary"
-                size="lg"
-                className="w-full"
-            >
-                <FcGoogle className="mr-2 size-5" />
-                Login with Google
-            </Button>
-            <Button
-                disabled={false}
-                variant="secondary"
-                size="lg"
-                className="w-full"
-            >
-                <FaGithub className="mr-2 size-5" />
-                Login with GitHub
-            </Button>
-        </CardContent>
-    </Card>                    
+                <Button
+                    disabled={false}
+                    variant="secondary"
+                    size="lg"
+                    className="w-full"
+                >
+                    <FaGithub className="mr-2 size-5" />
+                    Login with GitHub
+                </Button>
+            </CardContent>
+        </Card>                    
     );
-};        
-    
+};
 
 
-
-
+                            
+                            
                 
