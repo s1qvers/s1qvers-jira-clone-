6d774bc77c72d562727837a7ee392fd9f5ad9db3
s1qvers <s1qvers@gmail.com>
2024-12-13 00:45:38 +0600

    s1qvers

--- src/lib/session-middleware.ts
+++ src/lib/session-middleware.ts
@@ -17,7 +17,7 @@ import { createMiddleware } from "hono/factory";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
-type AdditinalContext = {
+type AdditionalContext = {
     Variables: {
         account: AccountType;
         databases: DatabasesType;
@@ -27,7 +27,7 @@ type AdditinalContext = {
     };
 };
 
-export const sessionMiddleware = createMiddleware<AdditinalContext>(
+export const sessionMiddleware = createMiddleware<AdditionalContext>(
     async (c, next) => {
         const client = new Client()
             .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -47,11 +47,12 @@ export const sessionMiddleware = createMiddleware<AdditinalContext>(
 
         const user = await account.get();
 
-        c.set("account,account);
-        c.set("databases, databases);
-        c.set("storage, storage);
-        c.set("user, user);
+        c.set("account", account);
+        c.set("databases", databases);
+        c.set("storage", storage);
+        c.set("user", user);
             
-         await next();   
+        await next();   
     },
 );
+
