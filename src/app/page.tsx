6f4ca60107bd9ceb1a7289376e1a92d54dea80da
s1qvers <s1qvers@gmail.com>
2024-12-13 00:55:26 +0600

    s1qvers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+"use client";
 
 export default function Home() {
- return (
+  const {  
+
+ 
+ 
+  return (
   <div className="">
-    <Input />
-    <Button size="xs">
-      Primary123
-    </Button>
-    <Button variant="secondary">
-      Secondary
-    </Button>
-    <Button variant="destructive">
-      Destructive
-    </Button>
-    <Button variant="ghost">
-      Ghost
-    </Button>
-    <Button variant="muted">
-      Muted
-    </Button>
-    <Button variant="outline">
-      Outline
-    </Button>
-    <Button variant="teritary">
-      Teritary
-    </Button>
+   
   </div>
   )
 }
