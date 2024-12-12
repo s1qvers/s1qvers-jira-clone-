9170826b9b3ff93ec1efd723968fad5bc34d1e70
s1qvers <s1qvers@gmail.com>
2024-12-13 01:59:39 +0600

    s1qvers

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,5 +1,5 @@
-import { useMutation, useQueries, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
 
@@ -8,18 +8,20 @@ type ResponseType = InferResponseType<typeof client.api.auth.loqout["$post"]>;
 export const useLogout = () => {
   const queryClient = useQueryClient();
 
-  const mutation = useMutation<
-    ResponseType,
-    Error
-  >({
+  const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.loqout["$post"](); 
       return await response.json();
     },
-});
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+  });
 
   return mutation;
 };
+
+
   
 
 
