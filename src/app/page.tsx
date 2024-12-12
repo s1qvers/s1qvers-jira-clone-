f4c8fed6d4453e81e9daf2c022ff8e12d839de0a
s1qvers <s1qvers@gmail.com>
2024-12-12 21:47:40 +0600

    s1qvers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+import { createAdminClient } from "@/lib/appwrite";
+
 export default function Home() {
+  console.log(createAdminClient, "hello");
  return (
   <div className="">
     <Input />
