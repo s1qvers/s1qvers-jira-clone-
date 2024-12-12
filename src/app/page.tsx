22ee5b1b9a452d8b9c904942af72dd2354fb0539
s1qvers <s1qvers@gmail.com>
2024-12-13 01:10:38 +0600

    s1qvers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-export default function Home() {
-  const {  
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
- 
- 
+import { useCurrent } from "@/features/auth/api/use-current";
+
+export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+  }
+}, [data]);
+
   return (
-  <div className="">
-   
-  </div>
+    <div className="">
+      Only visible to authorized users.
+    </div>
   )
 }
