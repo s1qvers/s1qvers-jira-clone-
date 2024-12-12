d7c2c6339eb4ffd5b97b68da680a03772e8e57e7
s1qvers <s1qvers@gmail.com>
2024-12-12 16:28:55 +0600

    s1qvers

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 
-import { loginSchema } from "../schemas";
+import { loginSchema, registerSchema } from "../schemas";
 
 const app = new Hono()
     .post(
@@ -15,6 +15,19 @@ const app = new Hono()
 
             return c.json({ email, password });
         }
-    );
+    )
+    .post(
+        "/register",
+        zValidator("json", registerSchema),
+        async (c) => {
+            const { email, password, } = c.req.valid("json");
+
+            console.log({ email, password });
+
+            return c.json({ email, password });
+        }
+    )
 
 export default app;
+
+
