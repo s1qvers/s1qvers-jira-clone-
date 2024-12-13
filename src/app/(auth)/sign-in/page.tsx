a53bf798756cc34f19dbd1e5a81ce82a7dfd0ea0
s1qvers <s1qvers@gmail.com>
2024-12-13 19:02:56 +0600

    s1qvers

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,6 +6,8 @@ import { SignInCard } from "@/features/auth/sign-in-card";
 const SignInPage = async () => {
     const user = await getCurrent();
 
+    console.log({ user })
+        
     if (user) redirect("/"); 
     
     return <SignInCard /> 
--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -15,6 +15,7 @@ export const getCurrent = async () => {
 
         if (!session) return null;
 
+        client.setSession(session.value);
         const account = new Account(client);
 
         return await account.get();
