7ba485747a434f4b9d36614171c5e92df10ba2e8
s1qvers <s1qvers@gmail.com>
2024-12-13 22:42:40 +0600

    s1qvers

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,10 +6,13 @@ interface DashboardLayoutProps {
     return (
       <div className="min-h-screen">
         <div className="flex w-full h-full">
+            <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
+                
+            </div>
           <div className="lg:pl-[264px]">
             <div className="mx-auto max-w-screen-2xl h-full">
                 {/* TODO: Navbar */}
-                <main>
+                <main className="h-full py-8 px-6 flex flex-col">
                     {children} 
                 </main>
             </div>
