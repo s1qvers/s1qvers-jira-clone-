ece6edd6f801e0d1ef996721183be329bfc21ee8
s1qvers <s1qvers@gmail.com>
2024-12-13 14:57:35 +0600

    s1qvers

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,12 +1,24 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { Client } from "node-appwrite";
+import { Account, Client } from "node-appwrite";
+
+import { AUTH_COOKIE } from "./constants";
 
 export const getCurrent = async () => {
-    const client = new Client()
-        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    try {
+        const client = new Client()
+            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-        const session = await cookies()
-}
+        const session = await cookies().get(AUTH_COOKIE);
+
+        if (!session) return null;
+
+        const account = new Account(client);
+
+        return await account.get();
+    }   catch {
+        return null;
+    }
+};
