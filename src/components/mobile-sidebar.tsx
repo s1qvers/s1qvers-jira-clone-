89ed7039f2334ba5bb69ccf3392c11bc45263741
s1qvers <s1qvers@gmail.com>
2024-12-14 00:43:07 +0600

    s1qvers

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,18 +1,27 @@
 "use client";
 
-import {MenuIcon} from "lucide-react"; 
+import { useState } from "react";
+import { MenuIcon } from "lucide-react"; 
 
+import { SideBar } from "./sidebar";
 import { Button } from "./ui/button"; 
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"; 
 
 export const MobileSidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const
+
     return (
         <Sheet modal={false}>
             <SheetTrigger asChild>
-                <Button size="icon" variant="secondary" className="lg:hidden">
+                <Button variant="secondary" className="lg:hidden">
                     <MenuIcon className="size-4 text-neutral-500" />
                 </Button>
             </SheetTrigger>
+            <SheetContent side="left" className="p-0"> 
+                <SideBar />
+            </SheetContent>
         </Sheet>
     );
 };
+
