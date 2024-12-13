05e0acb40c1eb6333b8fa370924d92b55874cfd3
s1qvers <s1qvers@gmail.com>
2024-12-13 13:39:48 +0600

    s1qvers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 
 import { useLogout } from "@/features/auth/api/use-logout";
 import { useCurrent } from "@/features/auth/api/use-current";
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter(); 
@@ -20,10 +20,7 @@ export default function Home() {
 
   return (
     <div>
-      Доступно только для авторизованных пользователей.
-      <Button onClick={() => mutate()}>
-        Loqout
-      </Button> 
+      <UserButton />
     </div>
   );
 }
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -18,9 +18,42 @@ export const UserButton = () => {
 
     if (isLoading) {
         return (
-            <div className="size-10 routed-full flex items-center justify-center bg-neutral-200 border border-neutral-300">
-                <Loader />
+            <div className="size-10 rounded-full flex items-center justify-center bg-neutral-200 border border-neutral-300">
+                <Loader className="size-4 animate-spin text-muted-foreground" />
             </div>
         );
-    }   
+    }
+
+    if (!user) {
+        return null;
+    }
+
+    const { name, email } = user;
+
+    const avatarFallback = name
+        ? name.charAt(0).toUpperCase()
+        : email.charAt(0).toUpperCase() ?? "U";
+
+    return (
+        <DropdownMenu modal={false}>
+        <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+                {avatarFallback}
+            </AvatarFallback>            
+        </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" side="bottom" className="w-60" sideOffset={10}>
+            <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+                <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
+                    <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+                        {avatarFallback}
+                    </AvatarFallback>            
+                </Avatar>
+            </div>
+        </DropdownMenuContent>
+    </DropdownMenu>
+    );
 };
+
+
