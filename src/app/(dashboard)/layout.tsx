3071327742a316c49c923cbd34c0ab2b9765be5a
s1qvers <s1qvers@gmail.com>
2024-12-13 22:36:08 +0600

    s1qvers

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,7 +7,12 @@ interface DashboardLayoutProps {
       <div className="min-h-screen">
         <div className="flex w-full h-full">
           <div className="lg:pl-[264px]">
-            {children}
+            <div className="mx-auto max-w-screen-2xl h-full">
+                {/* TODO: Navbar */}
+                <main>
+                    {children} 
+                </main>
+            </div>
           </div>
         </div>
       </div>
