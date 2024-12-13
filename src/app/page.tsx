ba687b48348bd69b23c3342f43523fa0bf1f8214
s1qvers <s1qvers@gmail.com>
2024-12-13 15:19:15 +0600

    s1qvers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { getCurrent } from "@/features/auth/actions";
 import { UserButton } from "@/features/auth/components/user-button";
 
 export default async function Home() {
-  const user = getCurrent();
-
-  console.log({ user });
+  const user = await getCurrent();
 
   if (!user) redirect("/sign-in");
 
