cafd536c83893c8585a015e4aaefe094be1a71d1
s1qvers <s1qvers@gmail.com>
2024-12-17 00:32:18 +0600

    s1qvers

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -3,6 +3,7 @@
 import { RiAddCircleFill } from "react-icons/ri";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspace";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 import {
     Select,
@@ -28,7 +29,10 @@ export const WorkspaceSwitcher = () => {
                 <SelectContent>
                     {workspaces?.documents.map((workspace) => (
                         <SelectItem key={workspace.$id} value={workspace.$id}>
-                            {workspace.name}
+                            <div className="flex justify-start items-center gap-3 font-medium">
+                                <WorkspaceAvatar name={workspace.name} image={workspace.imageUrl} />
+                                <span className="truncate">{workspace.name}</span>
+                            </div>
                         </SelectItem>
                     ))}
                 </SelectContent>
