49148d2d749e9d248a12153ad9ba4c011d69121f
s1qvers <s1qvers@gmail.com>
2024-12-20 01:34:25 +0600

    s1qvers

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"; 
 
-import { getCurrent } from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 import { SignUpCard } from "@/features/auth/sign-up-card";
 
 const SignUpPage = async () => {
