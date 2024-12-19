958cee98ad2a2b830f81133d5a1f8c36ad0a1b21
s1qvers <s1qvers@gmail.com>
2024-12-19 23:39:02 +0600

    s1qvers

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
 
 interface WorkspaceIdSettingsPageProps {
     params: {
@@ -14,9 +16,15 @@ const WorkspaceIdSettingsPage = async ({
     const user = await getCurrent();
     if (!user) redirect("/sign-in");
     
+    const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+    if (!initialValues) {
+        redirect(`/workspaces/${params.workspaceId}`);
+    }
+
     return (
         <div>
-            WorkspaceIdSettingsPage: {params.workspaceId}
+            <EditWorkspaceForm initialValues={initialValues} />
         </div>
     );
 };
