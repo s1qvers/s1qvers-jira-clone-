c242c71106341b3acb8686c21952c17273ef26d5
s1qvers <s1qvers@gmail.com>
2024-12-20 02:27:24 +0600

    s1qvers

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -165,6 +165,13 @@ const app = new Hono()
       if (!member || member.role !== MemberRole.ADMIN) {
         return c.json({ error: "Unauthorized" }, 401);
       }
+
+      await databases.deleteDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+      );
+      
     }
   )
 
