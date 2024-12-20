6122b6f512b5335248a5e514c5145fddf634c06f
s1qvers <s1qvers@gmail.com>
2024-12-21 03:03:47 +0600

    s1qvers

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -1,5 +1,7 @@
+import  { useState } from "react";
+ 
+import { Button, type ButtonProps } from "@/components/ui/button";
 import { ResponsiveModal } from "@/components/responsive-modal";
-import { Button, ButtonProps } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
@@ -7,58 +9,47 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { useState } from "react";
 
 export const useConfirm = (
-	title: string,
+	title: string, 
 	message: string,
 	variant: ButtonProps["variant"] = "primary"
 ): [() => JSX.Element, () => Promise<unknown>] => {
-	const [promise, setPromise] = useState<{
-		resolve: (value: boolean) => void;
-	} | null>(null);
+	const [promise, setPromise] = useState<{ resolve: (value: boolean) => void} | null>(null);
 
-	const confirm = () => {
-		return new Promise((resolve) => {
-			setPromise({ resolve });
-		});
-	};
+const confirm = () => {
+	return new Promise((resolve) => {
+		setPromise({ resolve });
+	});
+};
 
-	const handleClose = () => {
-		setPromise(null);
-	};
+const handleClose = () => {
+	setPromise(null);
+};
 
-	const handleCancel = () => {
-		promise?.resolve(false);
-		handleClose();
-	};
+const handleConfirm = () => {
+	promise?.resolve(true);
+	handleClose();
+};
 
-	const handleConfirm = () => {
-		promise?.resolve(true);
-		handleClose();
-	};
+const handleCancel = () => {
+	promise?.resolve(false);
+	handleClose();
+};
 
-	const ConfirmDialog = () => (
+	const ConfirmationDialog = () => (
 		<ResponsiveModal open={promise !== null} onOpenChange={handleClose}>
-			<Card className="size-full border-none shadow-none">
+			<Card className="w-full h-full border-none shadow-none">
 				<CardContent className="pt-8">
 					<CardHeader className="p-0">
-						<CardTitle>{title}</CardTitle>
-						<CardDescription>{message}</CardDescription>
+					<CardTitle>{title}</CardTitle>
+					<CardDescription>{message}</CardDescription>
 					</CardHeader>
 					<div className="pt-4 w-full flex flex-col gap-y-2 lg:flex-row gap-x-2 items-center justify-end">
-						<Button
-							variant="outline"
-							className="w-full lg:w-auto"
-							onClick={handleCancel}
-						>
+						<Button onClick={handleCancel} variant="outline" className="w-full lg:w-auto">
 							Cancel
 						</Button>
-						<Button
-							variant={variant}
-							className="w-full lg:w-auto"
-							onClick={handleConfirm}
-						>
+						<Button onClick={handleConfirm} variant={variant} className="w-full lg:w-auto">
 							Confirm
 						</Button>
 					</div>
@@ -67,5 +58,5 @@ export const useConfirm = (
 		</ResponsiveModal>
 	);
 
-	return [ConfirmDialog, confirm];
+	return [ConfirmationDialog, confirm];
 };
